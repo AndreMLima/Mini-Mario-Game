0f00fc384a258af9af7c94ba6c77903edc438fcb
André Lima <andreluis017@gmail.com>
2022-06-23 10:44:51 -0300

    Sets jumo animation and the scenario

---
+++ script.js
@@ -0,0 +1,11 @@
+const mario = document.querySelector('.super-mario');
+const pipe = document.querySelector('.pipe-game');
+
+const jump = () => {
+    mario.classList.add('jump-mario')
+    setTimeout(() =>{
+        mario.classList.remove('jump-mario');
+    }, 500)
+}
+
+document.addEventListener('keydown', jump);
